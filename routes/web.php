27ec042d16a6b7d50c09aa4a8559f57ac9c99939
lamishaorin <lamisha.rahman@g.bracu.ac.bd>
2024-12-19 21:53:52 +0600

    ..

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
+use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Controllers\Backend\PropertyTypeController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -35,46 +34,58 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+//Admin Group Middleware
 Route::middleware(['auth','role:admin'])->group(function(){
 Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-
+Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])->name('admin.update.password');
 }); //End Group Admin Middleware
 
 
 
-
+//Agent Group Middleware
 Route::middleware(['auth','role:agent'])->group(function(){
 Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 
 }); //End Group Agent Middleware
-Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
+Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name('agent.register');
 
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login')->middleware(RedirectIfAuthenticated::class);
   /// Admin Group Middleware 
-Route::middleware(['auth','role:admin'])->group(function(){
-    
-// Property Type All Route 
-Route::controller(PropertyTypeController::class)->group(function(){
-
-     Route::get('/all/type', 'AllType')->name('all.type')->middleware('permission:all.type'); 
-     Route::get('/add/type', 'AddType')->name('add.type')->middleware('permission:add.type');
-     Route::post('/store/type', 'StoreType')->name('store.type'); 
-     Route::get('/edit/type/{id}', 'EditType')->name('edit.type');
-     Route::post('/update/type', 'UpdateType')->name('update.type');
-     Route::get('/delete/type/{id}', 'DeleteType')->name('delete.type');  
-
-});
- // Amenities Type All Route 
- Route::controller(PropertyTypeController::class)->group(function(){
-    Route::get('/all/amenitie', 'AllAmenitie')->name('all.amenitie'); 
-    Route::get('/add/amenitie', 'AddAmenitie')->name('add.amenitie');
-    Route::post('/store/amenitie', 'StoreAmenitie')->name('store.amenitie'); 
-    Route::get('/edit/amenitie/{id}', 'EditAmenitie')->name('edit.amenitie');
-    Route::post('/update/amenitie', 'UpdateAmenitie')->name('update.amenitie');
-    Route::get('/delete/amenitie/{id}', 'DeleteAmenitie')->name('delete.amenitie'); 
-});
+  Route::middleware(['auth','roles:admin'])->group(function(){ 
 
 
+    // Property Type All Route 
+   Route::controller(PropertyTypeController::class)->group(function(){
+   
+        Route::get('/all/type', 'AllType')->name('all.type')->middleware('permission:all.type'); 
+        Route::get('/add/type', 'AddType')->name('add.type')->middleware('permission:add.type');
+        Route::post('/store/type', 'StoreType')->name('store.type'); 
+        Route::get('/edit/type/{id}', 'EditType')->name('edit.type');
+        Route::post('/update/type', 'UpdateType')->name('update.type');
+        Route::get('/delete/type/{id}', 'DeleteType')->name('delete.type');  
+   
+   });
+   
+   
+    // Amenities Type All Route 
+   Route::controller(PropertyTypeController::class)->group(function(){
+   
+        Route::get('/all/amenitie', 'AllAmenitie')->name('all.amenitie'); 
+        Route::get('/add/amenitie', 'AddAmenitie')->name('add.amenitie');
+        Route::post('/store/amenitie', 'StoreAmenitie')->name('store.amenitie'); 
+        Route::get('/edit/amenitie/{id}', 'EditAmenitie')->name('edit.amenitie');
+        Route::post('/update/amenitie', 'UpdateAmenitie')->name('update.amenitie');
+        Route::get('/delete/amenitie/{id}', 'DeleteAmenitie')->name('delete.amenitie');  
+   
+   });
+
+   }); // End Group Admin Middleware
 
 
-
+ 
