a70273f1b0c8a6ad8a91461f64a3c48237248e8c
lamishaorin <lamisha.rahman@g.bracu.ac.bd>
2024-12-19 22:20:46 +0600

    ..

---
+++ app/Http/Controllers/Backend/PropertyController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Property;
+use App\Models\MultiImage;
+use App\Models\Facility;
+
+class PropertyController extends Controller
+{
+    public function AllProperty(){
+        $property = Property::latest()->get();
+        return view('backend.property.all_property',compact('property'));
+    } // End Method   
+
+
+
+
+
+public function AddProperty(){
+
+    return view('backend.property.add_property');
+
+}// End Method 
+
+
+
+
+
+
+
+
+
+
+
+
+}
