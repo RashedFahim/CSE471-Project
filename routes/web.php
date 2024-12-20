509f26dd9dbac1f414fb7ef264a431219c694358
RashedFahim <167600600+RashedFahim@users.noreply.github.com>
2024-12-20 18:11:05 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -70,8 +70,6 @@ Route::get('/agent/logout', [AgentController::class, 'AgentLogout'])->name('agen
 }); //End Group Agent Middleware
 Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
 Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name('agent.register');
-
-
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login')->middleware(RedirectIfAuthenticated::class);
 
 
