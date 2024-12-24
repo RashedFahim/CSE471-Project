f497ed8c14cab72ee60c4f0f0a64e4bbaa4fdd83
lamishaorin <lamisha.rahman@g.bracu.ac.bd>
2024-12-24 18:51:14 +0600

    ,,

--- resources/views/agent/body/sidebar.blade.php
+++ resources/views/agent/body/sidebar.blade.php
@@ -48,12 +48,17 @@ $status = $agentId->status;
         
         
 
+        
+        
+        
+        
         <li class="nav-item">
-          <a href="pages/apps/calendar.html" class="nav-link">
+          <a href="{{ route('buy.package') }}" class="nav-link">
             <i class="link-icon" data-feather="calendar"></i>
-            <span class="link-title">Calendar</span>
+            <span class="link-title">Buy Package</span>
           </a>
         </li>
+        
         <li class="nav-item nav-category">Components</li>
         <li class="nav-item">
           <a class="nav-link" data-bs-toggle="collapse" href="#uiComponents" role="button" aria-expanded="false" aria-controls="uiComponents">
