2a962ba7f798f90609fb521dfc334b2294f6f5d9
lamishaorin <lamisha.rahman@g.bracu.ac.bd>
2024-12-24 12:57:16 +0600

    ..

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\Backend\PropertyTypeController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(PropertyTypeController::class)->group(function () {
+    Route::get('/new/type', 'NewType'); // Fetch all property types
+    Route::post('/save/type', 'SaveType'); // Create a new property type
 });
+
+
+
+
